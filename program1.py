aa5d208038a9d6469cc118e7257823f34fe988cb
Vinay Mirchandani <0105cs211134@oriental.ac.in>
2024-09-30 12:33:22 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -7,6 +7,10 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         count = 0
-        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
                     
         return 0
