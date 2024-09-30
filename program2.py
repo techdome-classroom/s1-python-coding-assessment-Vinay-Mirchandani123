caa2df7a69c581e2f50c36577ee55e3fd85869ef
Vinay Mirchandani <0105cs211134@oriental.ac.in>
2024-09-30 13:10:20 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -8,13 +8,13 @@ def decode_message( s: str, p: str) -> bool:
         # Initialize dp for patterns with leading *
         for j in range(1, n + 1):
                 if p[j - 1] == '*':
-                dp[0][j] = dp[0][j - 1]
+                        dp[0][j] = dp[0][j - 1]
 
         for i in range(1, m + 1):
                 for j in range(1, n + 1):
                         if p[j - 1] == '*':
-                        dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
-                elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
-                        dp[i][j] = dp[i - 1][j - 1]
+                                dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+                        elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
+                                dp[i][j] = dp[i - 1][j - 1]
 
         return dp[m][n]
