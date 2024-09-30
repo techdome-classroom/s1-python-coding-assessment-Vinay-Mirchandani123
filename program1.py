ad59d9f5c639bed87ed9b9f6679ccbaf7804c11e
Vinay Mirchandani <0105cs211134@oriental.ac.in>
2024-09-30 12:52:47 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,14 +4,23 @@ class Solution:
     #    write your code here
         if not grid:
             return 0
-        rows = len(grid)
-        cols = len(grid[0])
-        count = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 'L':
-                    self.dfs(grid, i, j)
-                    count += 1
+        def dfs(grid, i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
+            return
+        grid[i][j] = 'W'  # Mark the landmass as visited
+        dfs(grid, i + 1, j)  # Check down
+        dfs(grid, i - 1, j)  # Check up
+        dfs(grid, i, j + 1)  # Check right
+        dfs(grid, i, j - 1)  # Check left
+
+    island_count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 'L':
+                dfs(grid, i, j)
+                island_count += 1
+
+    return island_count
                     
         return 0
     def dfs(grid, i, j):
