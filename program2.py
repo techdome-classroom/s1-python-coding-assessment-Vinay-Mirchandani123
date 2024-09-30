b8d3773f25f6932e4465671c05e779782b55a307
Vinay Mirchandani <0105cs211134@oriental.ac.in>
2024-09-30 12:49:01 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -3,7 +3,6 @@ def decode_message( s: str, p: str) -> bool:
 # write your code here
         countStar = 0
         countQuestion = 0
-        
         for i in range(len(p)-1,-1,-1):
             
             if p[i] == '*':
