55655643908d701ae0bf7635da1355af1f468ee8
Vinay Mirchandani <0105cs211134@oriental.ac.in>
2024-09-30 12:23:37 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -6,8 +6,6 @@ def decode_message( s: str, p: str) -> bool:
                 if p[i] == "*":
                      continue
                 if p[i] == "?":
-                    if i == len(s)-1:
-                        return False
                     continue
                 return False
             if p[i] == "*":
